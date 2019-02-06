032c34b33cf9f2c2439e78b11a5a1d417b32b3d3
Jorge <Jorge@192.168.0.15>
2019-02-05 23:52:39 -0200

    Primeira versão do projeto

---
+++ Calculadora/src/calculadora/App.java
@@ -0,0 +1,34 @@
+
+package calculadora;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Faculdades São José");
+		calcular();
+		validar(4, 5.6f);
+		
+	}
+	
+	/**
+	 * @Auhor: Jorge
+	 * @version: 1.0
+	 * @deprecated
+	 * @see: Esse método foi substituído pelo método {@link #validar(int x, float y) validar} 
+	 **/
+	public static void calcular(){
+		
+		
+	}
+
+	/**
+	 * @Auhor: Jorge
+	 * @version: 2.0
+	 * @param: int x, float y
+	 */
+	public static void validar(int x, float y){
+		
+	}
+
+}
